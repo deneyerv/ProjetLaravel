e7dccf3b12e1f67fb315d54e2494857276b9bec3
deneyerv <tooky1995@gmail.com>
2016-12-29 16:11:46 +0100

    index

--- resources/views/shop/welcome.blade.php
+++ resources/views/shop/welcome.blade.php
@@ -6,14 +6,98 @@
 
 
 @section('content')
-<<div class="row">
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="\laravel5\public\css\Pictures\montagne1.jpg" alt="iconee">
+                    <div class="caption">
+                        <h3>Thumbnail label</h3>
+                        <div class="description">Lorem ipmeeeiejedejikd  ehuidhedh hedhe de</div>
+                        <div class="pull-left price">
+                            150€
+                        </div>
+                        <div class="clearfix">
+                            <p><a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a> </p>
+                        </div>
+                    </div>
+                </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                            <img src="\laravel5\public\css\Pictures\montagne1.jpg" alt="iconee">
+                            <div class="caption">
+                                <h3>Thumbnail label</h3>
+                                <div class="description">Lorem ipmeeeiejedejikd  ehuidhedh hedhe de</div>
+                                <div class="pull-left price">
+                                    150€
+                                </div>
+                                <div class="clearfix">
+                                    <p><a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a> </p>
+                                </div>
+                            </div>
+                        </div>
+    </div>
+
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
-            <img src="C:\wamp64\www\laravel5\public\css\Pictures\test.jpg" alt="iconee">
+            <img src="\laravel5\public\css\Pictures\montagne1.jpg" alt="iconee">
             <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>Lorem ipm</p>
-                <p><a href="#" class="btn btn-primary" role="button">Ajouter</a> <a href="#" class="btn btn-default" role="button">Supprimer</a></p>
+                <div class="description">Lorem ipmeeeiejedejikd  ehuidhedh hedhe de</div>
+                <div class="pull-left price">
+                    150€
+                </div>
+                <div class="clearfix">
+                    <p><a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a> </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+            <img src="\laravel5\public\css\Pictures\montagne1.jpg" alt="iconee">
+            <div class="caption">
+                <h3>Thumbnail label</h3>
+                <div class="description">Lorem ipmeeeiejedejikd  ehuidhedh hedhe de</div>
+                <div class="pull-left price">
+                    150€
+                </div>
+                <div class="clearfix">
+                    <p><a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a> </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+            <img src="\laravel5\public\css\Pictures\montagne1.jpg" alt="iconee">
+            <div class="caption">
+                <h3>Thumbnail label</h3>
+                <div class="description">Lorem ipmeeeiejedejikd  ehuidhedh hedhe de</div>
+                <div class="pull-left price">
+                    150€
+                </div>
+                <div class="clearfix">
+                    <p><a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a> </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+            <img src="\laravel5\public\css\Pictures\montagne1.jpg" alt="iconee">
+            <div class="caption">
+                <h3>Thumbnail label</h3>
+                <div class="description">Lorem ipmeeeiejedejikd  ehuidhedh hedhe de</div>
+                <div class="pull-left price">
+                    150€
+                </div>
+                <div class="clearfix">
+                    <p><a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a> </p>
+                </div>
             </div>
         </div>
     </div>
